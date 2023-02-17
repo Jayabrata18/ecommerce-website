733dc5794198b3cc626e1196845ed9fb0c30fb77
Jayabrata18 <jayabrata180402@gmail.com>
2023-02-17 12:03:27 +0530

    hh

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,5 +2,5 @@ module.exports = {
   mongoURI:
     "mongodb+srv://joy:joy1234@cluster0.otzsr41.mongodb.net/?retryWrites=true&w=majority",
 
-  jwtSecret: "ecomm-secret",  
+  jwtSecret: "ecomm-secret", 
 };
--- backend/routes/authApi.js
+++ backend/routes/authApi.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authorization");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/keys");
+const { check, validationResult } = require("express-validator");
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -13,5 +17,53 @@ router.get("/", auth, async (req, res) => {
     console.error(error.message);
   }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      //   console.log(req.body);
+      const { email, password } = req.body;
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invaild username or password" }] });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invaild username or password" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.jwtSecret,
+        { expiresIn: 3600 * 24 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //   res.send("User created successfully");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
